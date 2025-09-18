0b92fd80f35561e5953870ea6938c3ca0dfa740a
Acer <theodpozzo@gmail.com>
2025-09-18 15:58:29 +0100

    created basic bookshelf functionality

--- core/models.py
+++ core/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+class Language(models.Model):
+    name = models.CharField(max_length=50)
+    letter = models.CharField(max_length=1)
+    colour = models.CharField(max_length=7, default="#000000")  # Hex colour code
+
+    def __str__(self):
+        return self.name
+
+    def get_letter(self):
+        return self.letter
+    
+    def get_colour(self):
+        return self.colour
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    link = models.URLField(max_length=200)
+    language = models.ForeignKey(Language, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.title
+
+    def get_link(self):
+        return self.link
+
+    def get_description(self):
+        return self.description
+    
+    def get_language(self):
+        return self.language
+    
+    def get_tag(self):
+        return (self.language.get_letter(), self.language.get_colour())
+
+    class Meta:
+        ordering = ['language__name', 'title']
+        
--- core/views.py
+++ core/views.py
@@ -1,7 +1,52 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Book, Language
+
+def example_view(request):
+    # Example
+    language1 = {
+        "name": "English",
+        "letter": "E",
+        "colour": "#FF5733",
+    }
+    language2 = {
+        "name": "French",
+        "letter": "F",
+        "colour": "#33C1FF",
+    }
+
+    book1 = {
+        "title": "Test Book 1",
+        "description": "This is a test book description.",
+        "language": language1
+    }
+    book2 = {
+        "title": "Test Book 2",
+        "description": "second description",
+        "language": language1
+    }
+    book3 = {
+        "title": "Test Book 3",
+        "description": "third description",
+        "language": language2
+    }
+    
+    books = [book1, book3, book2]
+    return books
+
+
 
 def home(request):
-    context = {"name": "Theo Dal Pozzo"}
+    books = example_view(request)
+    books = None
+
+    context = {
+        "name": "Theo Dal Pozzo",
+        "title": "Welcome to my portfolio website!",
+        "linkedin": "https://www.linkedin.com/in/theo-dal-pozzo/",
+        "github": "https://www.github.com/theodpozzo",
+        "email": "theo@dpozzo.com",
+        "books": books,
+        }
     return render(request, "core/home.html", context)
